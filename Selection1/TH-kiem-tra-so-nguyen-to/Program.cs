21ddb9cf45920202e72768d31c9b9a54f45659fc
unknown <nguyenducnhattung12i@gmail.com>
2020-07-02 11:44:39 +0700

    Up BT

---
+++ Selection1/TH-kiem-tra-so-nguyen-to/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TH_kiem_tra_so_nguyen_to
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 2)
+                Console.WriteLine(number + " is not a prime");
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(number + " is a prime");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not a prime");
+                }
+            }
+        }
+    }
+}
